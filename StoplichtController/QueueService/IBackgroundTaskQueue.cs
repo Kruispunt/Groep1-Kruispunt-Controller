33b2c55e835b2f810a00126705fc0cf6aa326df2
GreyHunt3r <svenimholz@icloud.com>
2024-03-24 16:20:20 +0100

    Added background task queue interface and a default implementation for the queue

---
+++ StoplichtController/QueueService/IBackgroundTaskQueue.cs
@@ -0,0 +1,8 @@
+namespace StoplichtController.QueueService;
+
+public interface IBackgroundTaskQueue
+{
+    ValueTask QueueBackgroundWorkItemAsync(Func<CancellationToken, ValueTask> workItem);
+    
+    ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(CancellationToken cancellationToken);
+}
