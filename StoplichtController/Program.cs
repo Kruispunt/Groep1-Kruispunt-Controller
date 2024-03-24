15f5f6457d2a4f8a6d2907c890da02c000bfd012
GreyHunt3r <svenimholz@icloud.com>
2024-03-24 17:14:15 +0100

    Queue task when W is pressed

--- StoplichtController/Program.cs
+++ StoplichtController/Program.cs
@@ -1,7 +1,32 @@
 using StoplichtController;
+using StoplichtController.QueueService;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+// Add services to the container.
+
+// Add the Worker service
+// builder.Services.AddHostedService<Worker>();
+
+// Add the MonitorLoop
+builder.Services.AddSingleton<MonitorLoop>();
+builder.Services.AddHostedService<QueuedHostedService>();
+builder.Services.AddSingleton<IBackgroundTaskQueue>(_ => 
+{
+    if (!int.TryParse(builder.Configuration["QueueCapacity"], out var queueCapacity))
+    {
+        queueCapacity = 100;
+    }
+
+    return new DefaultBackgroundTaskQueue(queueCapacity);
+});
+
 
 var host = builder.Build();
+
+// Start the MonitorLoop
+MonitorLoop monitorLoop = host.Services.GetRequiredService<MonitorLoop>()!;
+monitorLoop.StartMonitorLoop();
+
+
 host.Run();
