e1d97493be27764ce2d94fb7b2fc9a3a1d00f95d
GreyHunt3r <svenimholz@icloud.com>
2024-05-06 17:27:11 +0200

    Sending the correct message! also when there is no bus or pedestrian it is not sending the list

--- StoplichtController/Messages/CrossingStateMessageGenerator.cs
+++ StoplichtController/Messages/CrossingStateMessageGenerator.cs
@@ -20,39 +20,46 @@ public class CrossingStateMessageGenerator()
 
             foreach (var road in crossing.Value.Roads)
             {
-                var laneDict = new Dictionary<string, List<LightState>>
-                {
-                    {
-                        "Cars",
-                        road.Value.Lanes.OfType<CarLane>()
-                            .Select(lane => lane.Light.State)
-                            .ToList()
-                    },
-                    {
-                        "Cyclists", road.Value.Lanes.OfType<BikeLane>()
-                            .Select(
-                            lane => lane
-                                .Light.State)
-                            .ToList()
-                    },
-                    {
-                        "Pedestrians",
-                        road.Value.Lanes.OfType<PedestrianLane>()
-                            .Select(lane => lane.Light.State)
-                            .ToList()
-                    },
-                    {
-                        "Busses",
-                        road.Value.Lanes.OfType<BusLane>()
-                            .Select(lane => lane.Light.State)
-                            .ToList()
-                    }
-                };
+                var laneDict = new Dictionary<string, List<LightState>>();
 
-                roadDict.Add(road.Key, laneDict);
+                // Check if there are any cars
+                var carLanes = road.Value.Lanes.OfType<CarLane>().ToList();
+                if (carLanes.Any())
+                {
+                    laneDict["Cars"] = carLanes.Select(lane => lane.Light.State).ToList();
+                }
+
+                // Check if there are any cyclists
+                var bikeLanes = road.Value.Lanes.OfType<BikeLane>().ToList();
+                if (bikeLanes.Any())
+                {
+                    laneDict["Cyclists"] = bikeLanes.Select(lane => lane.Light.State).ToList();
+                }
+
+                // Check if there are any pedestrians
+                var pedestrianLanes = road.Value.Lanes.OfType<PedestrianLane>().ToList();
+                if (pedestrianLanes.Any())
+                {
+                    laneDict["Pedestrians"] = pedestrianLanes.Select(lane => lane.Light.State).ToList();
+                }
+
+                // Check if there are any buses
+                var busLanes = road.Value.Lanes.OfType<BusLane>().ToList();
+                if (busLanes.Any())
+                {
+                    laneDict["Busses"] = busLanes.Select(lane => lane.Light.State).ToList();
+                }
+
+                if (laneDict.Any())
+                {
+                    roadDict.Add(road.Key, laneDict);
+                }
             }
 
-            crossingDict.Add(crossing.Key, roadDict);
+            if (roadDict.Any())
+            {
+                crossingDict.Add(crossing.Key, roadDict);
+            }
         }
 
         var jsonString = JsonConvert.SerializeObject(crossingDict);
